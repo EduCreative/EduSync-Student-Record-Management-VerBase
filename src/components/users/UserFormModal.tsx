3d1d731b9ce41b65f0eed7aa4343d06a982557a1
Masroor Khan <108581855+EduCreative@users.noreply.github.com>
2025-10-31 23:47:30 +0500

    refactor: Update UserFormModal imports
    Removes unused ROLE_PERMISSIONS import from UserFormModal.

--- src/components/users/UserFormModal.tsx
+++ src/components/users/UserFormModal.tsx
@@ -7,7 +7,7 @@ import { NAV_LINKS } from '../../constants';
 import ImageUpload from '../common/ImageUpload';
 import { useToast } from '../../context/ToastContext';
 import { supabase } from '../../lib/supabaseClient';
-import { Permission, ROLE_PERMISSIONS } from '../../permissions';
+import { Permission } from '../../permissions';
 
 const LockIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
